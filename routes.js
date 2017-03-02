8f942689f8651db83a5b3c925282f245b2e233e8
EDA <eda@mac-mini-4.local>
2017-03-03 11:46:21 +1300

    crop images and add placeholder routes

--- routes.js
+++ routes.js
@@ -1,13 +1,43 @@
 var fs = require('fs')
 
 module.exports = {
-  getHome: getHome
+  getHome: getHome,
+  getArtProfile: getArtProfile,
+  getArtistProfile: getArtistProfile
 }
 
-function getHome(req, res) {
+// functions...
+
+function getHome (req, res) {
   res.render('home')
 }
 
+function getArtProfile (req, res) {
 
+}
 
-//functions...
+function linkArtProfile (req, res) {
+  // read name of image
+  // use name of image to populate link
+}
+
+function renderArtProfile (req, res) {
+  // take image name
+  // read object in database based on image name
+  // render page with information from db
+}
+
+function getArtistProfile (req, res) {
+
+}
+
+function linkArtistProfile (req, res) {
+  // read name of artist
+  // use name of artist to populate link
+}
+
+function renderArtistProfile (req, res) {
+  // take artist name
+  // read object in database based on artist name
+  // render page with information from db
+}
