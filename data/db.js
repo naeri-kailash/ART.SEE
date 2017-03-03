acbaa3fefdefb8f0a829a085323598c3a293ba4f
EDA <eda@mac-mini-3.local>
2017-03-03 13:57:54 +1300

    modified db.js

--- data/db.js
+++ data/db.js
@@ -8,14 +8,27 @@ module.exports = {
   getArtwork: getArtwork
 }
 
-function getArtist (artistID, cb) {
+function readArtist (artistID, cb) {
   fs.readFile('data/artist.json', 'utf8', function (err, data) {
     if (err) {
       return cb(err)
     }
     var json = JSON.parse(data)
     var result = json.find(function (a) {
-      return a.id === artistID
+      return a.id === artistId
+    })
+    cb(null, result)
+  })
+}
+
+function readArtwork (id, cb) {
+  fs.readFile('data/artwork.json', 'utf8', function (err, data) {
+    if (err) {
+      return cb(err)
+    }
+    var json = JSON.parse(data)
+    var result = json.find(function (a) {
+      return a.id === id
     })
     cb(null, result)
   })
